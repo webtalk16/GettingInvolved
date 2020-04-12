9f100117b4d7c01e293dac9905bcc99d03f2eaff
Jimmy Levy <jimmyjlevy@hotmail.com>
2020-04-12 11:10:21 +0300

    change to IFC logo

--- src/js/components/login.js
+++ src/js/components/login.js
@@ -5,7 +5,7 @@ class Login {
 
   loadLogin(){
     this.buildHtml();
-    this.checkPwa();
+    // this.checkPwa();
     console.log('Login component is loaded');
   }
 
@@ -20,39 +20,39 @@ class Login {
     rootEl.insertAdjacentHTML('beforeend', html);
   }
 
-  checkPwa () {
-    setTimeout(() => {
-      if (window.deferredPrompt) {
-        // add install button
-        const node = document.createElement('DIV');
-        node.id = 'btnPwa';
-        const textnode = document.createTextNode('Install on Home Screen');
-        node.appendChild(textnode);
-        rootEl.appendChild(node);
+  // checkPwa () {
+  //   setTimeout(() => {
+  //     if (window.deferredPrompt) {
+  //       // add install button
+  //       const node = document.createElement('DIV');
+  //       node.id = 'btnPwa';
+  //       const textnode = document.createTextNode('Install on Home Screen');
+  //       node.appendChild(textnode);
+  //       rootEl.appendChild(node);
 
-        const btnPwa = document.querySelector('#btnPwa');
-        btnPwa.addEventListener('click', e => {
-          window.addEventListener('appinstalled', e => {
-            console.log('app is now installed on this device, thank you and enjoy :)');
-          });
-          this.doPwaInstall();
-        });
-      }
-    }, 2000);
-  }
+  //       const btnPwa = document.querySelector('#btnPwa');
+  //       btnPwa.addEventListener('click', e => {
+  //         window.addEventListener('appinstalled', e => {
+  //           console.log('app is now installed on this device, thank you and enjoy :)');
+  //         });
+  //         this.doPwaInstall();
+  //       });
+  //     }
+  //   }, 2000);
+  // }
 
-  async doPwaInstall() {
-    window.deferredPrompt.prompt();
-    console.log('deferredPrompt' + window.deferredPrompt)
-    window.deferredPrompt.userChoice.then(function(choiceResult){
-      if (choiceResult.outcome === 'accepted') {
-        console.log('Your PWA has been installed');
-      } else {
-        console.log('User chose to not install your PWA');
-      }
-      window.deferredPrompt = null;
-    });
-  }
+  // async doPwaInstall() {
+  //   window.deferredPrompt.prompt();
+  //   console.log('deferredPrompt' + window.deferredPrompt)
+  //   window.deferredPrompt.userChoice.then(function(choiceResult){
+  //     if (choiceResult.outcome === 'accepted') {
+  //       console.log('Your PWA has been installed');
+  //     } else {
+  //       console.log('User chose to not install your PWA');
+  //     }
+  //     window.deferredPrompt = null;
+  //   });
+  // }
 }
 
 export { Login } 
